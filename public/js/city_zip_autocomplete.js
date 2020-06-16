92ee00528246ecb51a8aaf396178249c4709c158
Sixeye <srinath@srinath.fr>
2020-06-16 10:17:17 +0200

    ajax adresse autocomplete

---
+++ public/js/city_zip_autocomplete.js
@@ -0,0 +1,67 @@
+$("#registration_code_postal").autocomplete({
+    source: function (request, response) {
+        $.ajax({
+            url: "https://api-adresse.data.gouv.fr/search/?postcode="+$("input[name='registration[code_postal]']").val(),
+            data: { q: request.term },
+            dataType: "json",
+            success: function (data) {
+                var postcodes = [];
+                response($.map(data.features, function (item) {
+                    // Ici on est obligé d'ajouter les CP dans un array pour ne pas avoir plusieurs fois le même
+                    if ($.inArray(item.properties.postcode, postcodes) === -1) {
+                        postcodes.push(item.properties.postcode);
+                        return { label: item.properties.postcode + " - " + item.properties.city,
+                            city: item.properties.city,
+                            value: item.properties.postcode
+                        };
+                    }
+                }));
+            }
+        });
+    },
+    // On remplit aussi la ville
+    select: function(event, ui) {
+        $('#registration_ville').val(ui.item.city);
+    }
+});
+$("#registration_ville").autocomplete({
+    source: function (request, response) {
+        $.ajax({
+            url: "https://api-adresse.data.gouv.fr/search/?city="+$("input[name='registration[ville]']").val(),
+            data: { q: request.term },
+            dataType: "json",
+            success: function (data) {
+                var cities = [];
+                response($.map(data.features, function (item) {
+                    // Ici on est obligé d'ajouter les villes dans un array pour ne pas avoir plusieurs fois la même
+                    if ($.inArray(item.properties.postcode, cities) === -1) {
+                        cities.push(item.properties.postcode);
+                        return { label: item.properties.postcode + " - " + item.properties.city,
+                            postcode: item.properties.postcode,
+                            value: item.properties.city
+                        };
+                    }
+                }));
+            }
+        });
+    },
+    // On remplit aussi le CP
+    select: function(event, ui) {
+        $('#sarl_code_postal').val(ui.item.postcode);
+    }
+});
+$("#registration_adresse").autocomplete({
+    source: function (request, response) {
+        $.ajax({
+            url: "https://api-adresse.data.gouv.fr/search/?postcode="+$("input[name='registration[code_postal]']").val(),
+            data: { q: request.term },
+            dataType: "json",
+            success: function (data) {
+                response($.map(data.features, function (item) {
+                    return { label: item.properties.name, value: item.properties.name};
+                }));
+            }
+        });
+    }
+});
+
